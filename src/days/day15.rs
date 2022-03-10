585b2f30703e45dcd5a1ba845e04ff0e151b0c67
Dino Costantini <dino.costantini@gmail.com>
2022-03-10 19:52:39 +0000

    day 15

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -7,7 +7,7 @@ pub fn day15() {
     let destination = [lines.len() - 1, lines[0].len() - 1];
     let result = spf(&lines, destination);
 
-    let destination2 = [lines.len() * 50 - 1, lines[0].len() * 50 - 1];
+    let destination2 = [lines.len() * 5 - 1, lines[0].len() * 5 - 1];
     let result2 = spf(&lines, destination2);
 
     println!("DAY 15\nSolution 1: {}\nSolution 2: {}", result, result2);
@@ -18,7 +18,7 @@ fn spf(nodes: &Vec<Vec<u8>>, destination: [usize; 2]) -> u32 {
     visited.insert([0, 0]);
     let boundaries = [destination[0] + 1, destination[1] + 1];
 
-    let mut helper_frontier = HashSet::<[usize; 2]>::new();
+    let mut frontier_index = HashSet::<[usize; 2]>::new();
     let mut frontier: BinaryHeap<Reverse<(u32, [usize; 2])>> = neighborhood(&[0, 0], &boundaries)
         .into_iter()
         .map(|[x, y]| (get_node(nodes, [x, y]), [x, y]))
@@ -36,9 +36,9 @@ fn spf(nodes: &Vec<Vec<u8>>, destination: [usize; 2]) -> u32 {
                 .into_iter()
                 .filter(|n| !visited.contains(n))
             {
-                if !helper_frontier.contains(&[x, y]) {
+                if !frontier_index.contains(&[x, y]) {
                     frontier.push(Reverse((get_node(nodes, [x, y]) + next_weight, [x, y])));
-                    helper_frontier.insert([x, y]);
+                    frontier_index.insert([x, y]);
                 }
             }
         }
